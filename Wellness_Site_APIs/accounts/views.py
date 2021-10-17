774f7881201bbc70b12d13666d557196f65d7771
Vismay Vora <voravismay9@gmail.com>
2021-10-17 20:40:22 +0530

    Removed registartion error by using JsonResponse

--- Wellness_Site_APIs/accounts/views.py
+++ Wellness_Site_APIs/accounts/views.py
@@ -1,16 +1,13 @@
 from django.shortcuts import render
 from rest_framework import status
-from rest_framework.decorators import api_view,authentication_classes,permission_classes
-from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from accounts.serializers import UserSerializer
 from rest_framework.authtoken.models import Token
-from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
+from django.http import JsonResponse
 
 # Create your views here.
 @api_view(['POST',])
-@authentication_classes([TokenAuthentication])
-@permission_classes([IsAuthenticated])
+
 def register_view(request):
 
     if request.method == 'POST':
@@ -21,7 +18,7 @@ def register_view(request):
             data['response'] = "User has been successfully registered."
             data['username'] = User.email
             token = Token.objects.get(user = User).key
-            data['token'] = Token
+            data['token'] = token
         else:
             data = serializer.errors
-        return Response(data)
+        return JsonResponse(data)
