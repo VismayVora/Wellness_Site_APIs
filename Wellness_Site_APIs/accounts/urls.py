b7baf8c212515f43e71b5b7d41a60733b672206b
Vismay Vora <voravismay9@gmail.com>
2021-10-22 15:54:42 +0530

    Refactored code for email confirmation

--- Wellness_Site_APIs/accounts/urls.py
+++ Wellness_Site_APIs/accounts/urls.py
@@ -2,7 +2,6 @@ from django.urls import path,include
 from accounts.views import register_view, verification_view
 from rest_framework.authtoken.views import obtain_auth_token
 
-
 urlpatterns = [
     path('register/', register_view, name = 'Register'),
     path('verify/', verification_view, name = 'Confirm Registration'),
--- Wellness_Site_APIs/accounts/views.py
+++ Wellness_Site_APIs/accounts/views.py
@@ -1,31 +1,33 @@
 from django.shortcuts import render
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from accounts.serializers import UserSerializer
 from rest_framework.authtoken.models import Token
 from django.http import JsonResponse,HttpResponse
 from django.core.mail import send_mail
 from django.conf import settings
+from rest_framework.permissions import AllowAny
 
 # Create your views here.
 @api_view(['POST',])
-
+@permission_classes([AllowAny])
 def register_view(request):
 
     if request.method == 'POST':
         serializer = UserSerializer(data = request.data) 
         data = {}
         if serializer.is_valid():
-            #User = serializer.save()
-            #data['response'] = "User has been successfully registered."
-            #data['username'] = User.email
-            #token = Token.objects.get(user = User).key
-            #data['token'] = token
             global registration_data
             def registration_data():
                 return serializer
-            send_mail('Verification of your Account','Thank You for joining us! Copy-paste this link and continue to create your account. Link: " http://127.0.0.1:8000/verify/ " ','voravismay99@gmail.com',[serializer.validated_data['email'],], fail_silently=False)
-            data['Message'] = "Check your email! {}".format(serializer.validated_data['email'])
+            
+            subject = 'Verification of your Account'
+            from_email = 'voravismay99@gmail.com'
+            to_email = [serializer.validated_data['email'], ]
+            body = 'Thank You for joining us! Copy-paste this link and continue to create your account. Link: " http://127.0.0.1:8000/verify/"'
+            send_mail(subject, body, from_email, to_email, fail_silently=False)
+
+            data['Message'] = "Check your email! {}".format(to_email)
         else:
             data = serializer.errors
         return JsonResponse(data)
@@ -42,7 +44,6 @@ def verification_view(request):
             data['username'] = User.email
             token = Token.objects.get(user = User).key
             data['token'] = token
-            #send_mail('Verification of your Account','Thank You for joining us! Copy-paste this link to create your account.','voravismay99@gmail.com',[User.email,], fail_silently=False)
         else:
             data = serializer.errors
         return JsonResponse(data)
