71cd1149ff79934846e1ac3b4d7b1e4ee26c1d3c
Vismay Vora <voravismay9@gmail.com>
2021-10-29 22:24:21 +0530

    Applied NestedSerializer to view all user data in UserList

--- Wellness_Site_APIs/accounts/serializers.py
+++ Wellness_Site_APIs/accounts/serializers.py
@@ -1,3 +1,4 @@
+from logs.serializers import DietLogSerializer,WorkoutLogSerializer,HealthDataSerializer
 from rest_framework import serializers
 from logs.models import DietLog,WorkoutLog
 from django.contrib.auth import get_user_model
@@ -6,6 +7,10 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     #DietLog = serializers.PrimaryKeyRelatedField(many=True, queryset=DietLog.objects.all())
     #WorkoutLog = serializers.PrimaryKeyRelatedField(many=True, queryset=WorkoutLog.objects.all())
+    
+    DietLog = DietLogSerializer(many=True, read_only = True)
+    WorkoutLog = WorkoutLogSerializer(many=True, read_only = True)
+    HealthData = HealthDataSerializer()
 
     class Meta:
         model = User
